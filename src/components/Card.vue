1fd85a3a28c2d5e0b1df1e05a92992347d0041f3
Zoldyck <bcn980910@gmail.com>
2024-12-25 15:54:03 -0500

    "Add initial version of Card.vue component with Tailwind styling"

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { CardContent } from '../types/types';
+
+
+defineProps<{ cardContent: CardContent, bgColor: string, textColor:string }>()
+
+
+</script>
+
+<template>
+    <div :class="bgColor" class="rounded-md shadow-lg border border-default-100">
+        <div class="p-6">
+            <div class="mb-6">
+                <img :src="cardContent.imagenLink" class="">
+            </div>
+            <h3 :class="textColor" class="text-xl font-medium text-default-900 mb-6">{{ cardContent.title }}</h3>
+            <p :class="textColor" class="text-base text-default-500">{{ cardContent.text }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
