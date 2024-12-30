a9e72b0941cc399e82007bad034527fe3382bd8a
Zoldyck <bcn980910@gmail.com>
2024-12-29 21:06:02 -0500

    everything related to dark mode was removed to redo it with tailwind CCS

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -2,19 +2,19 @@
 import type { CardContent } from '../types/CardTypes';
 
 
-defineProps<{ cardContent: CardContent, bgColor: string, textColor:string }>()
+defineProps<{ cardContent: CardContent }>()
 
 
 </script>
 
 <template>
-    <div :class="bgColor" class="rounded-md shadow-lg border">
+    <div class="rounded-md shadow-lg border">
         <div class="p-6">
             <div class="mb-6">
                 <img :src="cardContent.imagenLink" class="object-cover w-16 h-16">
             </div>
-            <h3 :class="textColor" class="text-xl font-medium mb-6">{{ cardContent.title }}</h3>
-            <p :class="textColor" class="text-base">{{ cardContent.text }}</p>
+            <h3 class="text-xl font-medium mb-6">{{ cardContent.title }}</h3>
+            <p  class="text-base">{{ cardContent.text }}</p>
         </div>
     </div>
 </template>
