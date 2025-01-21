5a10d7bf06128bbc1ea8097c9cc001e1d10cf790
Zoldyck <bcn980910@gmail.com>
2025-01-21 18:42:56 -0500

    Using the CustomButton component to check its correct operation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,118 +1,43 @@
 <script setup lang="ts">
 import Card from '../components/Card.vue';
-import type { CardContent } from '../types/CardTypes';
+import type { ButtonType, CardContent } from '../types/CardTypes';
 import { useI18n } from 'vue-i18n';
 import { savedLocale } from '../i18n/i18n';
+import CustomButton from '../components/CustomButton.vue';
+import { PhoneArrowDownLeftIcon } from "@heroicons/vue/24/outline";
 
-const { getLocaleMessage } = useI18n();
+const { getLocaleMessage, t } = useI18n();
 
 const localeMessages = getLocaleMessage(savedLocale());
-
 const cardsContent: CardContent[] = <CardContent[]>localeMessages.cardsContent;
 </script>
 
-<template>
+<template>          
+    <CustomButton
+      :method="() => console.log('click')"
+      icon-custom-styles="text-white"
+      :Icon="PhoneArrowDownLeftIcon"
+      text-custom-styles="fixed bottom-3 right-3"
+      type="Icon" />
   <div class="flex gap-8 p-8 justify-center">
     <Card :cardContent="cardContent" v-for="cardContent, index in cardsContent" :key="index" />
   </div>
-  <div class="">
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum?
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum?
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum?
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum?
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum?
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br> Lorem
-      ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus enim vel officiis, nisi dolorum repellendus
-      corrupti, quos labore repudiandae voluptas quod iusto nemo facilis qui! Magni sunt alias maiores harum? <br>
-    </p>
+  <div>
+    <CustomButton
+      :method="(params: object) => {console.log('click: ', params)}"
+      :params="{valor1: 'valor1'}"
+      :text="t('home.buttonPrimary.text')" 
+      :type="<ButtonType> t('home.buttonPrimary.type')" />
+    <CustomButton
+      :method="() => console.log('click')" 
+      :text="t('home.buttonLarge.text')" 
+      :type="<ButtonType> t('home.buttonLarge.type')" /> 
+    <CustomButton 
+      :method="() => console.log('click')"
+      :text="t('home.buttonIconText.text')" 
+      :type="<ButtonType> t('home.buttonIconText.type')"      
+      :Icon="PhoneArrowDownLeftIcon"
+      icon-custom-styles="text-white"
+      text-custom-styles="text-white" /> 
   </div>
 </template>
