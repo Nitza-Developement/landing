2f3aa21dbd97e601e2ffea4a3ef09ae0c5ffa47f
Zoldyck <bcn980910@gmail.com>
2025-05-24 10:02:32 -0400

    feat: adjust Dropdown component layout for better responsiveness

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -71,7 +71,7 @@ onUnmounted(() => {
           </svg>
         </a>
         <button v-else
-          class="group w-1/2 px-4 py-2 justify-between flex items-center xl:hover:bg-white
+          class="group w-1/2 xl:w-full px-4 py-2 justify-between flex items-center xl:hover:bg-white
           hover:bg-slate-100 xl:dark:hover:bg-slate-950 dark:hover:bg-slate-900 rounded-md">
           <span :class="{ 'text-primary dark:text-primary': isOpen }" class="xl:py-1 group-hover:text-primary cursor-pointer dark:text-slate-300">
             {{ item.name }}
